3a27f861a596dbc56eba3d88e6d590f0953f6c93
Vinh <tanlevh1@gmail.com>
2021-07-11 23:13:26 +0700

    comment-blog-book

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -4,6 +4,7 @@ const UserController = require('../../controllers/api/user.controller');
 
 //Book Detail
 router.get('/delete-comment-review/:id', UserController.deleteCommentReview);
+router.put('/edit-comment-review/:id', UserController.editCommentReview);
 router.get('/rate-review', UserController.rateReview);
 router.post('/comment-review', UserController.postCommentReview);
 
@@ -11,5 +12,6 @@ router.post('/comment-review', UserController.postCommentReview);
 router.get('/delete-comment-blog/:id', UserController.deleteCommentBlog);
 router.get('/rate-blog', UserController.rateBlog);
 router.post('/comment-blog', UserController.postCommentBlog);
+router.put('/edit-comment-blog/:id', UserController.editCommentBlog);
 
 module.exports = router;
